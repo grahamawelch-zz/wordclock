4780a9b79458af6b07e170f450f612993c668891
Graham Welch <grahamawelch@gmail.com>
2016-07-29 20:54:17 -0700

    Finish code to make and parse API calls/responses.

--- requests.py
+++ requests.py
@@ -5,7 +5,7 @@ DEBUG = True
 
 WEATHER = ('http://api.wunderground.com/api/'
            '%(key)s/%(features)s/q/%(query)s.%(format)s')
-QUERY = 'autoip'
+WEATHER_QUERY = 'autoip'
 FORMAT = 'json'
 
 # You need to have a file called 'wunderground_key'
@@ -13,25 +13,27 @@ FORMAT = 'json'
 with open('wunderground_key', 'r') as key_file:
     KEY = key_file.read()
 
-TIME = 'http://www.timeapi.org/%(timezone)s/now'
+TIME = 'http://www.timeapi.org/%(timezone)s/now?%(query)s'
+TIME_QUERY = '\H:\M:\S:\D'
 
 HOURLY_FORECAST = 'hourly_forecast'
 
 WIND = 'wspd'
 TEMP = 'temp'
 ICON = 'icon'
-FCTTIME = 'FCTTIME'
+UNIT = 'english'
 
-FIELDS = [WIND, TEMP, ICON, FCTTIME]
 
 TIMEZONE = None if not DEBUG else 'PDT'
+CHECKED_TIMEZONE = ''
+TODAY = ''
 
 
 def BuildWeatherRequestUrl():
   return WEATHER % {
     'key': KEY,
     'features': 'hourly',
-    'query': QUERY,
+    'query': WEATHER_QUERY,
     'format': FORMAT,
   }
 
@@ -47,7 +49,8 @@ def BuildTimezoneRequestUrl():
 
 def BuildTimeRequestUrl(timezone):
   return TIME % {
-    'timezone': timezone
+    'timezone': timezone,
+    'query': TIME_QUERY,
   }
 
 
@@ -79,15 +82,71 @@ def MakeTimezoneRequest():
 
 def SetTimezone():
   global TIMEZONE
-  # Poor style :'(
   TIMEZONE = MakeTimezoneRequest()['current_observation']['local_tz_short']
+
+  global CHECKED_TIMEZONE
+  CHECKED_TIMEZONE = TODAY
 
 
 def MakeTimeRequest():
-  if not TIMEZONE:
+  if not TIMEZONE or CHECKED_TIMEZONE != TODAY:
     SetTimezone()
   return MakeRequest(
     BuildTimeRequestUrl(TIMEZONE),
     'samples/sample_time.txt',
     is_json=False
   )
+
+
+def ParseWeatherResp(weather_resp, specific_h):
+  '''We care about ICON, WIND[UNIT], and TEMP[UNIT].'''
+  hour = weather_resp[HOURLY_FORECAST][specific_h]
+  icon = hour[ICON]
+  wind = hour[WIND][UNIT]
+  temp = hour[TEMP][UNIT]
+
+  return (icon, wind, temp)
+
+
+def ParseTimeResp(time_resp):
+  split = time_resp.split(':')
+  hour = int(split[0])
+  minute = int(split[1])
+  second = int(split[2])
+  date = split[3]
+
+  # Set TODAY so we know to update TIMEZONE tomorrow
+  global TODAY
+  TODAY = date
+
+  # Check for AM or PM
+  if hour >= 12:
+    meridiem = 'PM'
+  else:
+    meridiem = 'AM'
+
+  # Handle Noon and Midnight
+  if hour > 12:
+    hour = hour - 12
+  if hour == 0:
+    hour = 12
+
+  return (hour, minute, second, meridiem)
+
+
+def GetWeather(specific_h=0):
+  return ParseWeatherResp(MakeWeatherRequest(), specific_h)
+
+
+def GetTime():
+  return ParseTimeResp(MakeTimeRequest())
+
+
+
+print GetWeather()
+print GetTime()
+
+
+
+
+
