5fe113dd59dae84307542e072864ef3d9acd0cc4
Graham Welch <grahamawelch@gmail.com>
2016-08-07 20:57:04 -0700

    Test new arrangement of led strip.

--- words.py
+++ words.py
@@ -59,12 +59,12 @@ with open('birthdays', 'r') as birthday_file:
 
 
 # Weather
-SUN = Weather('SUN', [1])
-CLOUD = Weather('CLOUD', [2])
-RAIN = Weather('RAIN', [3])
-STORM = Weather('STORM', [4])
-SNOW = Weather('SNOW', [5])
-WIND = Weather('WIND', [6])
+SUN = Weather('SUN', [8])
+CLOUD = Weather('CLOUD', [7])
+RAIN = Weather('RAIN', [6])
+STORM = Weather('STORM', [5])
+SNOW = Weather('SNOW', [4])
+WIND = Weather('WIND', [3])
 
 # List of possible Forecast Icons
 # https://www.wunderground.com/weather/api/d/docs?d=resources/phrase-glossary#forecast_description_phrases
@@ -114,36 +114,36 @@ def WeatherToEnums(icon, wind):
 
 
 # Minutes
-M_HALF = Word('HALF', [7, 8, 9])
-M_TWENTY = Word('TWENTY', [96, 98, 100])
-M_FIVE = Word('FIVE', [93, 95])
-M_QUARTER = Word('QUARTER', [14, 16, 18])
-M_TEN = Word('TEN', [11, 12, 13])
+M_HALF = Word('HALF', [0, 1, 2])
+M_TWENTY = Word('TWENTY', [12, 13, 14, 15])
+M_FIVE = Word('FIVE', [16, 17, 18])
+M_QUARTER = Word('QUARTER', [28, 27, 26, 25, 24])
+M_TEN = Word('TEN', [23, 23])
 
 # Transitions
-IT = Word('IT', [83])
-PAST = Word('PAST', [84, 86])
-IS = Word('IS', [87])
-TO = Word('TO', [88])
+IT = Word('IT', [32, 32])
+PAST = Word('PAST', [34, 35, 36])
+IS = Word('IS', [37])
+TO = Word('TO', [38])
 
 # Hours
-H_ONE = Word('ONE', [22])
-H_SIX = Word('SIX', [24])
-H_NINE = Word('NINE', [25, 27])
-H_THREE = Word('THREE', [75, 77])
-H_SEVEN = Word('SEVEN', [72, 74])
-H_ELEVEN = Word('ELEVEN', [34, 36, 38])
-H_FIVE = Word('FIVE', [31, 33])
-H_TEN = Word('TEN', [61, 62])
-H_FOUR = Word('FOUR', [63, 65])
-H_TWO = Word('TWO', [67])
-H_EIGHT = Word('EIGHT', [41, 43, 44])
-H_TWELVE = Word('TWELVE', [45, 47, 44])
+H_ONE = Word('ONE', [49, 48])
+H_SIX = Word('SIX', [47, 45])
+H_NINE = Word('NINE', [44, 43, 42])
+H_THREE = Word('THREE', [52, 53, 54, 55])
+H_SEVEN = Word('SEVEN', [56, 57, 58, 59])
+H_ELEVEN = Word('ELEVEN', [69, 68, 67, 66, 65])
+H_FIVE = Word('FIVE', [64, 63, 62])
+H_TEN = Word('TEN', [72, 73])
+H_FOUR = Word('FOUR', [74, 75, 76])
+H_TWO = Word('TWO', [77, 78])
+H_EIGHT = Word('EIGHT', [89, 88, 87, 86])
+H_TWELVE = Word('TWELVE', [86, 85, 84, 83, 82])
 
 # Other stuff
-OCLOCK = Word('OCLOCK', [54, 56, 58])
-AM = Word('AM', [52])
-PM = Word('PM', [51])
+OCLOCK = Word('OCLOCK', [91, 92, 93, 94, 95])
+AM = Word('AM', [96, 97])
+PM = Word('PM', [98, 99])
 
 
 def TimeToEnums(hour, minutes, seconds, meridiem):
