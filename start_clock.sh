a666b2095524a4106920339fcd0234990eef7244
Graham Welch <grahamawelch@gmail.com>
2016-08-22 21:21:06 -0700

    Reduce risk of disk corruption on shutoff by not writing errors to file.

--- start_clock.sh
+++ start_clock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/pi/workspace/wordclock
-sudo python /home/pi/workspace/wordclock/clock.py 2> /home/pi/workspace/wordclock/errors.txt
+sudo python /home/pi/workspace/wordclock/clock.py 
